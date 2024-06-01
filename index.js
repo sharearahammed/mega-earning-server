c847623aef09e307518be455ff7962a1793edb3a
Sharear Ahammed <sharearahammed@gmail.com>
2024-06-02 04:46:46 +0600

    get task route by email

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.netgysa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,7 +37,9 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const db = client.db("megaEarningDB");
-    const usersCollection = db.collection('users')
+    const usersCollection = db.collection("users");
+    const taskCollection = db.collection("tasks");
+    const feedbackCollection = db.collection("feedback");
 
     // auth related api
     app.post("/jwt", async (req, res) => {
@@ -50,47 +52,100 @@ async function run() {
       res.send({ token });
     });
     // Logout
-    app.post("/logout", async (req, res) => { 
-      const user = req.body; 
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
       // console.log("logging out", user);
       res
         .clearCookie("token", { ...cookieOptions, maxAge: 0 })
         .send({ success: true });
     });
 
+    // get user feedback
+    app.get("/feedback", async (req, res) => {
+      const result = await feedbackCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a user
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
     // save a user data in db
-    app.post('/users', async (req, res) => {  
-        const user = req.body
-  
-        const query = { email: user?.email }
-        // check if user already exists in db
-        const isExist = await usersCollection.findOne(query)
-        if (isExist) {
-            // if existing user login again
-            return res.send(isExist)
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user?.email };
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        // if existing user login again
+        return res.send(isExist);
+      }
+
+      let initialCoinValue = 0;
+      if (user.role === "Worker") {
+        initialCoinValue = 10;
+      } else if (user.role === "TaskCreator") {
+        initialCoinValue = 50;
+      }
+
+      // save user for the first time
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...user,
+          coins: initialCoinValue,
+          timestamp: Date.now(),
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    // add tasks in taskCollection
+    app.post("/addTask", async (req, res) => {
+      try {
+        const task = req.body;
+        const result = await taskCollection.insertOne(task);
+
+        const email = task.taskCreator.email;
+        const task_quantity = task.task_quantity;
+        const payable_amount = task.payable_amount;
+        console.log({ email, task_quantity, payable_amount });
+        const totalDeduction = task_quantity * payable_amount;
+
+        const updateResult = await usersCollection.updateOne(
+          { email: email },
+          { $inc: { coins: -totalDeduction } }
+        );
+        if (updateResult.matchedCount === 0) {
+          return res.status(404).send({ error: "User not found" });
         }
 
-        let initialCoinValue = 0;
-        if (user.role === 'Worker') {
-          initialCoinValue = 10;
-        } else if (user.role === 'TaskCreator') {
-          initialCoinValue = 50;
-        }
-  
-        // save user for the first time
-        const options = { upsert: true }
-        const updateDoc = {
-          $set: {
-            ...user,
-            coins: initialCoinValue,
-            timestamp: Date.now(),
-          },
-        }
-        const result = await usersCollection.updateOne(query, updateDoc, options)
-        res.send(result)
-      })
+        res.send({ result, updateResult });
+      } catch (err) {
+        console.error("Error adding task or updating user's coins:", err);
+        res.status(500).send({ error: "An error occurred" });
+      }
+    });
+    // get tasks in taskCollection
+    app.get("/task/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "taskCreator.email": email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
+    // delete one task by id
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
