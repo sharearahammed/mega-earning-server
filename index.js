e8365bacfa510ab58c858ce4feb6f888a35c421b
Sharear Ahammed <sharearahammed@gmail.com>
2024-06-02 04:47:42 +0600

    delete task by id

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ async function run() {
       res.send(result);
     });
 
-    // delete one task by id
+    // delete one task by id 
     app.delete("/tasks/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
